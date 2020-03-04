ed70d946d1146874efd4414c71c7df0a003de8d7
Balázs Laczkó <laczko.balazs89@gmail.com>
2020-03-04 10:09:12 +0100

    adds fighting elements

--- game/GameFighting.java
+++ game/GameFighting.java
@@ -4,6 +4,9 @@ public class GameFighting {
 	public static boolean fighting(PlayerDatas.Player player, Enemies.Enemy enemy) {
 		System.out.println(player.name + enemy.name);
 		return true;
+		
+		if (player.init > enemy.init) {
+		}
 	}
 	
 }
